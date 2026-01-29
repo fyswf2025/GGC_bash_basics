ddf179547378f9aa3f08435c519360cf6cefbe26
Youngse <fyswf2025@gmail.com>
2026-01-29 15:27:11 -0500

    add my name bash script

---
+++ 01-read-input-youngse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello Youngse Cho!"
+echo "How old are you?"
+read age
+echo 'Thanks Youngse! I see you are ' $age ' years old'
+
+echo "What school do you currently attend?"
+read school
+echo 'Nice! ' $school ' sounds like a great place to learn.'
